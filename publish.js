af61ea3d13952b8bcfd400fae924e6ecd0292518
911tanhua <911tanhua@gmail.com>
2026-06-18 21:30:33 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,14 +37,14 @@ var emails = [
 ];
 
 var urls=[]; 
-       urls.push(".akcnypqb.cc");  
+       urls.push(".lbgbfyab.cc");  
+       urls.push(".akcnypqb.cc");
        urls.push(".yrjochqxh.com");
-       urls.push(".yqjmrekvl.cc");
        
           
       
                                                                                  
-var JumpPage="https://esyvmtrw.cc";
+var JumpPage="https://pnrrkvp.cc";
 
 var newestUrls = [];
 
